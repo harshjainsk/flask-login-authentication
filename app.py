2acc49e8083d36d17c9e92234a7ce94f88c3587d
harshjainsk <harshjainsk531@gmail.com>
2023-03-21 11:20:26 +0530

    added flaskform

--- app.py
+++ app.py
@@ -1,6 +1,68 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
+
+# creating a flask instance
 app = Flask(__name__)
+
+
+#connecting to database
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+
+# creating a databaser instance
+db = SQLAlchemy(app)
+
+
+
+"""
+    add secret key to keep tha cookie data safe
+    In production we have to explicitly define 
+    and keep it confidential
+"""
+app.config['SECRET_KEY'] = 'thisIsSecretKEy'
+
+# create table for user
+
+"""
+    for table creation use flask shell
+"""
+class User(db.Model, UserMixin):
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), nullable = False, unique = True)
+    password = db.Column(db.String(80), nullable = False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+        
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
 
 @app.route('/')
 def home():
